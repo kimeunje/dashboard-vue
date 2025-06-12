8959ab133c96b040dec27d746de1efe3ccf1e051
kimeunje <world.wide.eunseo@gmail.com>
2025-06-12 23:57:35 +0900

    점검 결과 수정 중중

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -74,23 +74,25 @@ def register_controllers(app):
     from app.controllers.security_education_controller import education_bp
     from app.controllers.phishing_training_controller import training_bp
     from app.controllers.total_score_controller import score_bp
-    from app.controllers.admin_controller import admin_bp
     from app.controllers.admin_exception_controller import exception_bp
     from app.controllers.training_period_controller import training_period_bp
     from app.controllers.manual_check_controller import manual_check_bp
-    from app.controllers.manual_check_period_controller import manual_check_period_bp  # 추가
-
+    from app.controllers.manual_check_period_controller import (
+        manual_check_period_bp,
+    )  # 추가
 
     app.register_blueprint(auth_bp, url_prefix="/api/auth")
     app.register_blueprint(audit_bp, url_prefix="/api/security-audit")
     app.register_blueprint(education_bp, url_prefix="/api/security-education")
     app.register_blueprint(training_bp, url_prefix="/api/phishing-training")
     app.register_blueprint(score_bp, url_prefix="/api/security-score")
-    app.register_blueprint(admin_bp, url_prefix="/api/admin")
     app.register_blueprint(exception_bp, url_prefix="/api/exceptions")
     app.register_blueprint(training_period_bp, url_prefix="/api/phishing-training")
     app.register_blueprint(manual_check_bp, url_prefix="/api/manual-check")  # 새로 추가
-    app.register_blueprint(manual_check_period_bp, url_prefix="/api/manual-check")  # 추가
+    app.register_blueprint(
+        manual_check_period_bp, url_prefix="/api/manual-check"
+    )  # 추가
+
 
 def register_error_handlers(app):
     """에러 핸들러 등록"""
